59eeb714655e31a415d6ab5ca75e8de0c9c36d1f
david <david@unknown3c970ea6502a.lan>
2020-07-05 22:23:36 +0100

    Started the spider

--- stock_crawler/spiders/yahoo_finance_screener_spider.py
+++ stock_crawler/spiders/yahoo_finance_screener_spider.py
@@ -1,1 +1,26 @@
-a = 1
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class YFScreener(CrawlSpider):
+    name = 'YFScreener'
+    allowed_domains = ['https://finance.yahoo.com/screener/unsaved/c97bc7b4-0e94-43dc-9df1-b46f936742e6?count=25&offset=0']
+    start_urls = ['http://www.example.com']
+
+    rules = (
+        # Extract links matching 'category.php' (but not matching 'subsection.php')
+        # and follow links from them (since no callback means follow=True by default).
+        Rule(LinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
+
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        Rule(LinkExtractor(allow=('https://finance.yahoo.com/screener/.*count=\d+&offset=\d+')), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        self.logger.info('Hi, this is an item page! %s', response.url)
+        item = scrapy.Item()
+        item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+        item['name'] = response.xpath('//td[@id="item_name"]/text()').get()
+        item['description'] = response.xpath('//td[@id="item_description"]/text()').get()
+        item['link_text'] = response.meta['link_text']
+        return item
